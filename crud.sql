56d90eab9bd0a364736b78fd12d0d7afc85d3f09
lfeitosaf <lucas-jpl@hotmail.com>
2022-12-03 00:13:09 -0300

    add crud, selects, tables sql

--- crud.sql
+++ crud.sql
@@ -5,30 +5,63 @@
 
 -- 1)
 
-
+INSERT INTO 
+	clientes(nome, lealdade)
+VALUES('Georgia', 0)
 
 -- 2)
 
-
+INSERT INTO pedidos
+    (status, cliente_id)
+VALUES
+  ('Recebido', 6);
 
 -- 3)
 
-
+INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+VALUES
+  (6, 1),
+  (6, 2),
+  (6, 6),
+  (6, 8),
+  (6, 8);
 
 -- Leitura
 
 -- 1)
 
-
+SELECT
+ 	cl,
+ 	pe,
+ 	pr
+ FROM 
+ 	pedidos pe
+ JOIN
+ 	produtos_pedidos pp ON pe.id = pp.pedido_id 
+ JOIN 
+ 	produtos pr ON pr.id = pp.produto_id
+ JOIN 
+ 	clientes cl ON cl.id = pe.cliente_id 
+ WHERE
+	cl.nome = 'Georgia';
 
 -- Atualização
 
 -- 1)
 
-
+UPDATE 
+	clientes 
+SET 
+	lealdade = 48
+WHERE
+	nome = 'Georgia';
 
 -- Deleção
 
 -- 1)
 
-
+DELETE FROM
+	clientes
+WHERE 
+	nome = 'Marcelo';
--- selects.sql
+++ selects.sql
@@ -1,12 +1,74 @@
--- Aqui você deve colocar os códigos SQL referentes às
+	-- Aqui você deve colocar os códigos SQL referentes às
 -- Seleções de dados
 
 -- 1)
 
+SELECT
+ 	pe,
+ 	pr 
+ FROM 
+ 	pedidos pe
+ JOIN
+ 	produtos_pedidos pp ON pe.id = pp.pedido_id 
+ JOIN 
+ 	produtos pr ON pr.id = pp.produto_id;
+
+
 -- 2)
+
+SELECT
+ 	pe.id
+ FROM 
+ 	pedidos pe
+ JOIN
+ 	produtos_pedidos pp ON pe.id = pp.pedido_id 
+ JOIN 
+ 	produtos pr ON pr.id = pp.produto_id
+ WHERE
+	pr.nome ILIKE '%Fritas%';
 
 -- 3)
 
+SELECT
+ 	cl.nome gostam_de_fritas
+ FROM 
+ 	pedidos pe
+ JOIN
+ 	produtos_pedidos pp ON pe.id = pp.pedido_id 
+ JOIN 
+ 	produtos pr ON pr.id = pp.produto_id
+ JOIN 
+ 	clientes cl ON cl.id = pe.cliente_id 
+ WHERE
+	pr.nome ILIKE '%Fritas%';
+
 -- 4)
 
+SELECT
+ 	ROUND(SUM(pr.preço)::NUMERIC,2) sum
+ FROM 
+ 	pedidos pe
+ JOIN
+ 	produtos_pedidos pp ON pe.id = pp.pedido_id 
+ JOIN 
+ 	produtos pr ON pr.id = pp.produto_id
+ JOIN 
+ 	clientes cl ON cl.id = pe.cliente_id 
+ WHERE
+	cl.nome = 'Laura';
+
 -- 5)
+
+  SELECT
+ 	pr.nome nome,
+ 	COUNT(nome) count
+ FROM 
+ 	pedidos pe
+ JOIN
+ 	produtos_pedidos pp ON pe.id = pp.pedido_id 
+ JOIN 
+ 	produtos pr ON pr.id = pp.produto_id
+ GROUP BY 
+	nome
+ ORDER BY
+	nome;
